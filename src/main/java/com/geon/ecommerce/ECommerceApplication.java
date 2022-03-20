c3c98d3d8c8ceb39444e752ff072fc540b27866d
my <jvckigh@gmail.com>
2022-03-20 15:59:19 +0900

    블록하운드 적용

--- src/main/java/com/geon/ecommerce/ECommerceApplication.java
+++ src/main/java/com/geon/ecommerce/ECommerceApplication.java
@@ -2,11 +2,18 @@ package com.geon.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class ECommerceApplication {
 
     public static void main(String[] args) {
+        BlockHound.builder()
+                  .allowBlockingCallsInside(
+                          TemplateEngine.class.getCanonicalName(), "process")
+                  .install();
+
         SpringApplication.run(ECommerceApplication.class, args);
     }
 
